b7b8b78e918d88b0b023c4d650db3ee951aa3120
mikarin0901 <mikarin0901@gmail.com>
2026-08-16 07:09:38 +0900

    売却済み商品の編集制限と価格計算を修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -40,7 +40,7 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    redirect_to root_path if current_user.id != @item.user_id
+    redirect_to root_path if current_user.id != @item.user_id || @item.order.present?
   end
 
   private
