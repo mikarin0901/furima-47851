6b2b7b8ee4cd68aef90b355f2dce023f98dc2ac6
mikarin0901 <mikarin0901@gmail.com>
2026-08-15 22:41:09 +0900

    商品購入機能を実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,7 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
 
   belongs_to :user
+  has_one :order
   belongs_to :category
   belongs_to :sales_status
   belongs_to :shipping_fee_status
